4b618da7e008bfe3c160db12a813638106961fe8
tuanna-1205 <tuanna-1205>
2022-02-28 11:45:59 +0700

    Build Skeleton

--- react-pr2/src/features/Product/components/ProductFilters.jsx
+++ react-pr2/src/features/Product/components/ProductFilters.jsx
@@ -1,13 +1,17 @@
 import { Box } from '@mui/system';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import FilterByCategory from './Filters/FilterByCategory';
 import FilterByPrice from './Filters/FilterByPrice';
 import FilterByService from './Filters/FilterByService';
+import ProductSkeletonCategory from './ProductSkeletonCategory';
 
 function ProductFilters(Props) {
+  const isLoading = useSelector((state) => state.products.isLoading);
+
   return (
     <Box>
-      <FilterByCategory />
+      {isLoading ? <ProductSkeletonCategory length={6} /> : <FilterByCategory />}
       <FilterByPrice />
       <FilterByService />
     </Box>
